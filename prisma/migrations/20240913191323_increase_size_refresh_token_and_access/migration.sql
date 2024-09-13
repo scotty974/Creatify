ce387b60ee3accf4e497c9627da13944f5911fe2
scotty974 <fabienethevepro@gmail.com>
2024-09-13 21:16:06 +0200

    add routes to connect and to create client account

---
+++ prisma/migrations/20240913191323_increase_size_refresh_token_and_access/migration.sql
@@ -0,0 +1,3 @@
+-- AlterTable
+ALTER TABLE `user` MODIFY `accessToken` LONGTEXT NULL,
+    MODIFY `refreshToken` LONGTEXT NULL;
--- routes/auth/index.js
+++ routes/auth/index.js
@@ -23,7 +23,7 @@ router.post("/api/register", async (req, res) => {
     },
   });
   if (existingUser) {
-    return res.status(400).send("Utilisateur existant");
+    return res.status(400).json({ message: "Un compte existe déjà !" });
   }
 
   const passwordHash = await argon2.hash(data.password);
@@ -40,4 +40,76 @@ router.post("/api/register", async (req, res) => {
   });
 });
 
+router.post("/api/login", async (req, res) => {
+  let data;
+  try {
+    data = loginSchema.parse(req.body);
+  } catch (error) {
+    return res.status(400).json(error);
+  }
+
+  const user = await prisma.user.findFirst({
+    where: {
+      email: data.email,
+    },
+  });
+  if (!user) {
+    return res.json(400).json({ message: "Utilisateur introuvable ! " });
+  }
+
+  const verifyPassword = argon2.verify(user.password, data.password);
+
+  if (!verifyPassword) {
+    return res
+      .json(400)
+      .json({ message: "Mot de passe ou Email incorrect ! " });
+  }
+
+  const accessToken = generateAccessToken(user);
+  const refreshToken = generateRefreshToken(user);
+
+  await prisma.user.update({
+    where: {
+      email: data.email,
+    },
+    data: {
+      refreshToken: refreshToken,
+      accessToken: accessToken,
+    },
+  });
+
+  res.send({ accessToken: accessToken, refreshToken: refreshToken });
+});
+
+router.post("/api/client-register", async (req, res) => {
+  let data;
+  try {
+    data = loginSchema.parse(req.body);
+  } catch (error) {
+    return res.status(400).send(error);
+  }
+
+  const existingUser = await prisma.user.findFirst({
+    where: {
+      email: data.email,
+    },
+  });
+  if (existingUser) {
+    return res.status(400).json({ message: "Un compte existe déjà !" });
+  }
+
+  const passwordHash = await argon2.hash(data.password);
+
+  await prisma.user.create({
+    data: {
+      email: data.email,
+      password: passwordHash,
+      role: "CLIENT",
+    },
+  });
+
+  return res.status(200).json({
+    message: "Votre compte est bien enregistré ! ",
+  });
+});
 export default router;
