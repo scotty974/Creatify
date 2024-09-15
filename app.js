99bfe7e6885bcfd17c1e23ff28ccd933798bb914
scotty974 <fabienethevepro@gmail.com>
2024-09-15 19:32:49 +0200

    add new routes to create tags

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import express from "express";
 import auth from "./routes/auth/index.js";
 import profil from "./routes/profil/index.js";
 import media from './routes/posts/index.js'
-
+import tag from './routes/tags/index.js'
 const app = express();
 const port = 3000;
 app.use(express.json());
@@ -11,6 +11,7 @@ app.use("/uploads", express.static("uploads"))
 app.use("/", auth);
 app.use("/", profil);
 app.use('/', media)
+app.use('/', tag)
 
 app.listen(port, () => {
   console.log("Server is running on port " + port);
--- routes/tags/index.js
+++ routes/tags/index.js
@@ -9,21 +9,71 @@ const auth = expressjwt({
   algorithms: ["HS256"],
 });
 
-router.post('/api/tags', auth, async(req, res)=>{
-    let data;
-    try {
-        data = tagsValidation.parse(req.body)
-    } catch (error) {
-        return res.status(403).json({message : 'Une erreur est survenue'})
+router.post("/api/tags", auth, async (req, res) => {
+  let data;
+
+  try {
+    // Valider les données
+    data = tagsValidation.parse(req.body);
+  } catch (error) {
+    return res.status(403).json({ message: "Données invalides" });
+  }
+
+  try {
+    // Récupérer le profil de l'utilisateur connecté
+    const profil = await prisma.profil.findUnique({
+      where: {
+        userId: req.auth.id, // userId est la référence de l'utilisateur dans le modèle Profil
+      },
+    });
+
+    // Vérifier si le profil existe
+    if (!profil) {
+      return res.status(404).json({ message: "Profil non trouvé" });
     }
 
+    // Créer un tag avec le profilId récupéré
     await prisma.tags.create({
-        data : {
-            name : data.name
-        }
-    })
-    return res.status(200).json({message : 'Tag bien enregistré'})
-})
+      data: {
+        name: data.name,
+        profilId: profil.id, // Utiliser le profilId récupéré
+      },
+    });
 
+    return res.status(200).json({ message: "Tag bien enregistré" });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Une erreur est survenue lors de la création du tag" });
+  }
+});
 
+router.get("/api/tags/:name", auth, async (req, res) => {
+  const name = req.params.name;
+  try {
+    const tags = await prisma.tags.findMany({
+      where: {
+        name: {
+          contains: name
+        },
+      },
+      include: {
+        profil: true,
+      },
+    });
+    if (tags.length === 0) {
+      return res
+        .status(404)
+        .json({ message: "Aucun profil trouvé pour ce tag" });
+    }
+
+    // Extraire les profils des tags
+    const profiles = tags.map((tag) => tag.profil);
+
+    return res.status(200).json(profiles);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Une erreur est survenue" });
+  }
+});
 export default router;
